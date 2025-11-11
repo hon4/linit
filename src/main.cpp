26e09d6ec0609c9badcde134cb27398260e9a2ce
hon4 <greek.team.blog2@gmail.com>
2025-11-11 17:13:02 +0000

    Created Version Info

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,16 @@
 std::string linit_version = "0.0.1";
 std::map<std::string, std::string> conf = read_config();
 
+void show_ver();
+
 int main(int argc, char *argv[]) {
+	show_ver();
 	std::cout << "\rTest";
 	std::cout << std::flush << "\rTest2\n";
 	std::cout << conf["test"];
 	return 0;
+}
+
+void show_ver() {
+	std::cout << "\nlinit " << linit_version << "\n=============\n\nCoded by: hon\nLanguage: C++\n\n";
 }
