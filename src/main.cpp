6a2324ce3835f17a3204c3725f19c61c1b25478a
hon4 <greek.team.blog2@gmail.com>
2025-11-11 16:53:25 +0000

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <map>
+#include "inc/config.hpp"
 
-int main() {
-	std::cout << "Test\n";
+std::string linit_version = "0.0.1";
+std::map<std::string, std::string> conf = read_config();
+
+int main(int argc, char *argv[]) {
+	std::cout << "\rTest";
+	std::cout << std::flush << "\rTest2\n";
+	std::cout << conf["test"];
 	return 0;
 }
