de52705a7fce825f7a7d6d0ed6be69a1e09a4b91
hon4 <greek.team.blog2@gmail.com>
2025-11-20 11:58:33 +0000

    Rename function

--- src/inc/conf_handler.hpp
+++ src/inc/conf_handler.hpp
@@ -1,6 +1,6 @@
 #ifndef LINIT_CONF_HANDLER_HPP
 #define LINIT_CONF_HANDLER_HPP
 
-std::map<std::string, std::string> read_conf(std::string path);
+std::map<std::string, std::string> conf_read(std::string path);
 
 #endif
