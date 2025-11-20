a7a3e9c03d1cabdb82a695019a3965006523a076
hon4 <greek.team.blog2@gmail.com>
2025-11-20 11:53:17 +0000

    Create conf file handler

---
+++ src/inc/conf_handler.cpp
@@ -0,0 +1,17 @@
+#include <map>
+#include <fstream>
+
+std::map<std::string, std::string> read_conf(std::string path) {
+	std::map<std::string, std::string> ret;
+	std::ifstream oppened_file(path);
+	std::string line;
+
+	while (std::getline(oppened_file, line)) {
+		if (line.empty() || line[0] == "#") continue;
+		size_t xpos = line.find('=');
+		if (xpos == std::string::npos) continue; //Skip Bad formatted lines
+		std::string key = line.substr(0, xpos);
+		ret[key] = line.substr(xpos + 1);
+	}
+	return ret;
+}
